7e8665edfcf63e47cf1f7be069395690add97720
Marcos Caputo <caputo.marcos@gmail.com>
2024-05-16 17:58:16 -0300

    Atualizações na API de pedidos para suporte a filtros e correções de tipos
     Este commit introduz várias melhorias e correções na documentação e no código da API de pedidos. As mudanças incluem a adição de parâmetros d
     filtro para as rotas de obtenção de pedidos da memória e do banco de dados, bem como a correção do tipo de dados para o upload de arquivos e
     para os parâmetros de filtro.
    
     Alterações detalhadas:
     - Corrigido o tipo de dados do campo 'file' de 'string' para 'file' na documentação da API e no código, permitindo o upload de arquivos
     binários.
     - Adicionados parâmetros de filtro 'userId', 'orderId', 'startDate' e 'endDate' na rota GET '/memory/orders' para permitir a filtragem de
     pedidos armazenados na memória.
     - Adicionados parâmetros de filtro 'userId' e 'orderId' na rota GET '/database/orders' para permitir a filtragem de pedidos armazenados no
     banco de dados.
     - Corrigida a descrição da resposta 400 para a rota POST '/upload' para refletir a possibilidade de exceder o número máximo de arquivos.
     - Adicionada a resposta 400 para a rota GET '/database/orders' para casos em que nenhum filtro é especificado.
     - Adicionada a resposta 404 para as rotas GET '/memory/orders' e GET '/database/orders' para casos em que nenhum pedido é encontrado com os
     filtros fornecidos.
     - Removida a linha em branco no final do arquivo 'swagger.json'.
    
     Essas atualizações visam melhorar a funcionalidade e a usabilidade da API, fornecendo maior flexibilidade e precisão na recuperação de dados
     pedidos.

--- MagaLu/src/api/Routes.js
+++ MagaLu/src/api/Routes.js
@@ -20,13 +20,13 @@ const router = express.Router();
  *               file:
  *                 type: array
  *                 items:
- *                   type: string
+ *                   type: file
  *                   format: binary
  *     responses:
  *       200:
  *         description: Lista de usuários processados a partir dos arquivos carregados.
  *       400:
- *         description: Nenhum arquivo foi carregado.
+ *         description: Nenhum arquivo foi carregado ou o número máximo de arquivos excedido.
  */
 router.post("/upload", upload.array("file", 10), orderController.processUpload);
 
@@ -35,12 +35,33 @@ router.post("/upload", upload.array("file", 10), orderController.processUpload);
  * /memory/orders:
  *   get:
  *     summary: Obter pedidos da memória
- *     description: Retorna a lista de pedidos armazenados na memória.
+ *     description: Retorna a lista de pedidos armazenados na memória com base nos filtros fornecidos.
+ *     parameters:
+ *       - in: query
+ *         name: userId
+ *         schema:
+ *           type: number
+ *         description: ID do usuário para filtrar os pedidos.
+ *       - in: query
+ *         name: orderId
+ *         schema:
+ *           type: number
+ *         description: ID do pedido para filtrar os pedidos.
+ *       - in: query
+ *         name: startDate
+ *         schema:
+ *           type: string
+ *         description: Data de início para filtrar os pedidos (formato YYYY-MM-DD).
+ *       - in: query
+ *         name: endDate
+ *         schema:
+ *           type: string
+ *         description: Data de fim para filtrar os pedidos (formato YYYY-MM-DD).
  *     responses:
  *       200:
- *         description: Lista de pedidos da memória.
+ *         description: Lista de pedidos da memória filtrada.
  *       404:
- *         description: Nenhum pedido encontrado na memória.
+ *         description: Nenhum pedido encontrado com os filtros fornecidos.
  */
 router.get("/memory/orders", orderController.getOrders);
 
@@ -54,16 +75,20 @@ router.get("/memory/orders", orderController.getOrders);
  *       - in: query
  *         name: userId
  *         schema:
- *           type: string
+ *           type: number
  *         description: ID do usuário para filtrar os pedidos.
  *       - in: query
  *         name: orderId
  *         schema:
- *           type: string
+ *           type: number
  *         description: ID do pedido para filtrar os pedidos.
  *     responses:
  *       200:
  *         description: Lista de pedidos do banco de dados.
+ *       400:
+ *         description: Nenhum filtro foi especificado para a busca.
+ *       404:
+ *         description: Nenhum pedido encontrado com os filtros fornecidos.
  *       500:
  *         description: Erro ao buscar pedidos.
  */
@@ -74,22 +99,32 @@ router.get("/database/orders", async (req, res) => {
 
     // Construir o objeto de consulta com base nos filtros fornecidos
     if (filters.userId) {
-      query.userId = filters.userId; // Filtrar por ID do usuário
+      query.userId = Number(filters.userId); // Filtrar por ID do usuário
     }
 
-    // Adicionar mais condições conforme necessário com base nos campos disponíveis no modelo
     if (filters.orderId) {
-      query["orders.orderId"] = filters.orderId; // Filtrar por ID do pedido
+      // Filtrar por ID do pedido dentro do array de pedidos
+      query.orders = { $elemMatch: { orderId: Number(filters.orderId) } };
+    }
+
+    // Verificar se nenhum filtro foi setado
+    if (Object.keys(query).length === 0) {
+      return res.status(400).json({ message: "Nenhum filtro foi especificado para a busca." });
     }
 
     // Buscar pedidos com base na consulta construída
-    const orders = await UserOrder.find(query); // Usar find() sem argumentos retorna todos os documentos
-    res.status(200).json(orders); // Retornar os pedidos encontrados
+    const orders = await UserOrder.find(query);
+    if (orders.length === 0) {
+      return res.status(404).json({ message: "Nenhum pedido encontrado com os filtros fornecidos." });
+    } else {
+      res.status(200).json(orders); // Retornar os pedidos encontrados
+    }
   } catch (error) {
     console.error("Erro ao buscar pedidos:", error); // Log de erro ao buscar pedidos
     res.status(500).json({ error: "Erro ao buscar pedidos" }); // Retornar erro ao buscar pedidos
   }
 });
+
 
 /**
  * @openapi
